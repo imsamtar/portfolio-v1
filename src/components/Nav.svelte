486b25f7d4d6eb59921bb746266662002b0f1af7
Sameer Tariq <imsamtar@gmail.com>
2019-07-30 12:38:03 +0500

    Changed home link href to '/#' from '/'

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -23,7 +23,7 @@
 
 <nav class="bg-black z-10 sticky top-0 h-24" style="top:{showNavbar?'0':'-5.7rem'};">
     <ul class="sm:flex justify-around h-24">
-        <li class="flex w-full p-3 sm:p-0"><a class="text-center w-full text-white sm:p-8 font-bold text-lg cursor-pointer hover:bg-white hover:text-black" href="/">Home</a></li>
+        <li class="flex w-full p-3 sm:p-0"><a class="text-center w-full text-white sm:p-8 font-bold text-lg cursor-pointer hover:bg-white hover:text-black" href="/#">Home</a></li>
         <li class="flex w-full p-3 sm:p-0"><a class="text-center w-full text-white sm:p-8 font-bold text-lg cursor-pointer hover:bg-white hover:text-black" href="#projects">Projects</a></li>
         <li class="flex w-full p-3 sm:p-0"><a class="text-center w-full text-white sm:p-8 font-bold text-lg cursor-pointer hover:bg-white hover:text-black" href="#skills">Skills</a></li>
         <li class="flex w-full p-3 sm:p-0"><a class="text-center w-full text-white sm:p-8 font-bold text-lg cursor-pointer hover:bg-white hover:text-black" href="#contact">Contact</a></li>
